89648065a94d0c5c532052f7bd0d52a9c76ee171
Yalcin Kurt <yalcin.kurt89@gmail.com>
2023-07-27 17:19:31 +0200

    Added challenge solution

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,10 +4,31 @@ public class App {
     
     public static void main(String[] args) {
     
-    }
+        // Erzeugen von 3 Instanzen von Konto
+        Konto konto1 = new Konto();
+        Konto konto2 = new Konto();
+        Konto konto3 = new Konto();
 
 
-    private static void output(String outputStr) {
+        // Ausgabe des Anfangskontostands
+        output(konto1.kontostand);
+        output(konto2.kontostand);
+        output(konto3.kontostand);
+
+
+        // Ändern der Kontostände
+        konto1.setKontostand(konto1.getKontostand() * 2);
+        konto2.setKontostand(konto2.getKontostand() * 3);
+        konto3.setKontostand(konto3.getKontostand() * 10);
+
+        // Ausgabe der aktuellen Kontostände
+        output(konto1.getKontostand());
+        output(konto2.getKontostand());
+        output(konto3.getKontostand());
+       }
+
+
+    private static void output(int outputStr) {
         System.out.println(outputStr);
     } 
 
---
+++ com/cc/java/Konto.java
@@ -0,0 +1,14 @@
+package com.cc.java;
+
+public class Konto {
+    
+    public int kontostand = 10000;
+
+    public int getKontostand() {
+        return kontostand;
+    }
+
+    public void setKontostand(int kontostand) {
+        this.kontostand = kontostand;
+    }
+}
