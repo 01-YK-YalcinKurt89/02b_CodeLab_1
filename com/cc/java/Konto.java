b1af5f97173c216392dce2705387aded9478f657
Yalcin Kurt <yalcin.kurt89@gmail.com>
2023-07-31 23:36:00 +0200

    added some comments

--- com/cc/java/Konto.java
+++ com/cc/java/Konto.java
@@ -1,17 +1,23 @@
 package com.cc.java;
 
-public class Konto {
+public class Konto { // Private Instanzvariable zur Speicherung des Kontostands
 
     private int kontostand;
 
+
+// Konstruktor zur Initialisierung des Kontostands beim Erstellen eines Konto-Objekts
     public Konto(int kontostand) {
         this.kontostand = kontostand;
     }
 
+
+// Getter-Methode, um den aktuellen Kontostand abzurufen
     public int getKontostand() {
         return kontostand;
     }
 
+    
+// Setter-Methode, um den Kontostand zu aktualisieren
     public void setKontostand(int kontostand) {
         this.kontostand = kontostand;
     }
